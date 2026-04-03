f9aed63f56ecf896cf1a8a5cfa49acfca5a2b8dc
RaghavArora2 <raghavarora419@gmail.com>
2026-04-03 16:34:45 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: "/app", destination: "/download" },
+      { source: "/app/:path*", destination: "/download/:path*" },
+    ];
+  },
 };
 
 export default nextConfig;
